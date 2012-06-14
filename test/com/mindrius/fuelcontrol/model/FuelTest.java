430fa08dc701997d0506fb71ad54fe9e34913542
Eduardo Sztokbant <du@du.net.br>
2012-06-14 20:14:22 -0300

    JUnit infrastructure

---
+++ test/com/mindrius/fuelcontrol/model/FuelTest.java
@@ -0,0 +1,14 @@
+package com.mindrius.fuelcontrol.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FuelTest {
+
+	@Test
+	public void dummyTest() {
+		assertEquals(42, 42);
+	}
+
+}
